15dbb2276a84ba02f127af92577dea32837e6c18
Noe Olvera <olvera.alan@gmail.com>
2023-01-10 13:45:19 -0600

    chore!: install splash screen

--- ios/reactNativePaperStarter/AppDelegate.mm
+++ ios/reactNativePaperStarter/AppDelegate.mm
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import "RNSplashScreen.h"  // here
 
 #import <React/RCTAppSetupUtils.h>
 
@@ -57,6 +58,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
+  [RNSplashScreen show];  // here
   return YES;
 }
 
